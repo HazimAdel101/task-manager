8b1e78f7217b447308e21c16419ad912e82cd21c
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2024-04-04 14:18:38 -0800

    Add vue-toastification

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -9,18 +9,26 @@
 
 import { ref } from 'vue';
 import { useTasks } from '@/stores/TaskStore';
+import { useToast } from "vue-toastification";
+
 const taskStore = useTasks();
 const newTask = ref('');
+const toast = useToast();
+
 const handleSubmit = () => {
 
-    if (newTask.value.length > 0){
+    if (newTask.value.length > 0) {
         taskStore.addTask({
             title: newTask.value,
             isFav: false,
             id: Math.floor(Math.random() * 999999)
         })
-
         newTask.value = '';
+    }
+    else {
+        toast.error("The Name is required", {
+            timeout: 2000
+        });
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
-
 app.use(createPinia())
+app.use(Toast);
 
 app.mount('#app')
